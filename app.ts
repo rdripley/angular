e7ce05a3d9fec2e259124074bd00f2e348ecb127
Russ Ripley <russellripley@hotmail.com>
2017-02-24 14:11:25 -0700

    fix router

--- app.ts
+++ app.ts
@@ -3,6 +3,25 @@ namespace MyApp {
   // Angular Module
   angular.module('MyApp', ['ui-router']).config(($stateProvider: ng.ui.IStateProvider,
     $locationProvider: ng.ILocationProvider, $urlRouterProvider: ng.ui.IUrlRouterProvider) => {
-
+      $stateProvider
+      .state('Home', {
+                url: '/',
+                templateUrl: "ngApp/page1.html",
+                controller: MyApp.Controllers.Page1Controller,
+                controllerAs: 'controller'
+            })
+            .state('Page1', {
+                url: '/page1',
+                templateUrl: "ngApp/page1.html",
+                controller: MyApp.Controllers.Page1Controller
+            })
+            .state('Page2', {
+                url: '/page2',
+                templateUrl: "ngApp/page2.html",
+                controller: MyApp.Controllers.Page2Controller,
+                controllerAs: 'controller'
+            });
+        $urlRouterProvider.otherwise('/');
+        $locationProvider.html5Mode(true);
     });
 }
